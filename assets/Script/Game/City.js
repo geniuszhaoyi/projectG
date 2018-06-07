134fad3f344fee8cc9aed295676c1960f98a6f7b
geniuszhaoyi <genius.zhaoyi@gmail.com>
2018-06-06 22:16:24 -0400

    m

--- assets/Script/Game/City.js
+++ assets/Script/Game/City.js
@@ -1,11 +1,21 @@
-
+var Global = require("./../../Global").storage;
 
 class City {
-    constructor(id, name, position, to, npcs) {
+    constructor(id, name, position, to) {
         this.id = id;
         this.name = name;
         this.position = position;
         this.to = to;
+        // if(to === undefined) this.to = []
+    }
+    getNpcList() {
+        var res = [];
+        for(var npc of Global.Game.npcs) {
+            if(npc.isInCity(this.id)) {
+                res.push(npc);
+            }
+        }
+        return res;
     }
 }
 
