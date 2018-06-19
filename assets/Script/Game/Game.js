0a5e8851886b90a78bf7db16942137fdded9759c
geniuszhaoyi <genius.zhaoyi@gmail.com>
2018-06-19 09:02:43 -0400

    a

--- assets/Script/Game/Game.js
+++ assets/Script/Game/Game.js
@@ -13,8 +13,9 @@ var Buff = require("Buff");
 class Game {
     cities = {
         city_001: new City("city_001", "Home", new Position(10, 10, "bg_001"), [{id: "city_002", requiredItems: ["key_001"]}]),
-        city_002: new City("city_002", "Yard", new Position(120, 10, "bg_001"), [{id: "city_001"},{id: "city_003"}]),
-        city_003: new City("city_003", "Street", new Position(120, 120, "bg_001"), [{id: "city_001"},{id: "city_002"}]),
+        city_002: new City("city_002", "Yard", new Position(120, 10, "bg_001"), [{id: "city_001"}, {id: "city_003"}]),
+        city_003: new City("city_003", "Street", new Position(120, 120, "bg_001"), [{id: "city_001"}, {id: "city_002"}, {id: "city_004", requiredItems: ["weapon_001"]}]),
+        city_004: new City("city_004", "Cave", new Position(-300, 180, "bg_001"), []),
     }
     items = {
         item_001:   new Item("item_001", "Moldy Book", "An old moldy book. You can hardly read the writing. "),
@@ -29,6 +30,7 @@ class Game {
     npcs = {
         npc_001: new Npc_001(),
         npc_002: new Npc_Item('npc_002', {name: 'Old Well', description: 'It has been abandon for years. Looks like something is down there. '}, 'city_003', 'weapon_001', 1),
+        npc_003: new Npc_Monster('npc_003', {name: '哥布林', description: '一只看起来很弱的哥布林', enemy: 'enemy_001', drop: [{item: 'item_001', quantity: 1, possibility: 50}]}),
     }
     enemies = {
         enemy_001: new Enemy([150, 1000, 0, 0, 0, 0, 1000, 0, 0], ['skill_001']),
