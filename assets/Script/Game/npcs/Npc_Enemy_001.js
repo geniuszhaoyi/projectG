4663d02cb2fc625acccca89376028ae35a15d480
kanbd <kanbdsh@gmail.com>
2018-06-21 18:41:29 -0400

    sd

---
+++ assets/Script/Game/npcs/Npc_Enemy_001.js
@@ -0,0 +1,36 @@
+var Npc = require ('./../Npc.js');
+var Action = require ('./../Action.js');
+
+class Npc_Enemy_001 extends Npc {
+    constructor(id, enemy) {
+        console.log("hello     "+enemy);
+        super(id, {name: enemy.name, description: enemy.description}, 'city_004');
+        this.actions = [new ActionEnemy(id, enemy)];
+    }
+}
+
+class ActionEnemy extends Action {
+    constructor(id, enemy) {
+        super('NpcMonster' +id, '打他', '你打了一下' + enemy.name + '，好像惹怒他了！');
+        this.enemy=enemy
+    }
+    start() {
+        return {
+            context: {title: '', text: this.context},
+            type: 'battle',
+            enemy: this.enemy,
+            callback: this._callback.bind(this)
+        };
+    }
+    _callback(message) {
+        if(message === true || message === 'true') {
+
+            return new Action();
+        }else {
+
+            return false;
+        }
+    }
+}
+
+module.exports = Npc_Enemy_001;
