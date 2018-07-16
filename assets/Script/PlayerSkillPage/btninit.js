9489463b7042f2802217bd444fbe64a5e597a8f5
kanbd <kanbdsh@gmail.com>
2018-07-15 22:06:56 -0400

    best

---
+++ assets/Script/PlayerSkillPage/btninit.js
@@ -0,0 +1,81 @@
+var skill1 = require("Battle/Game/skills/Skill_001");
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+        skill:null,
+        detial:{
+            default:null,
+            type:cc.Prefab,
+        },
+        maskPre:{
+            default:null,
+            type:cc.Prefab,
+        },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        var skilld=new skill1("skill_001");
+        this.seticon(skilld);
+        
+    },
+
+    seticon(skilld){
+        this.skill=skilld;
+        cc.loader.loadRes("Texture/Item/"+this.skill.id, function(err, data) {
+            console.log(skilld.id);
+            if (!err) {
+                this.spriteFrame = new cc.SpriteFrame(data);
+            }else {
+                cc.loader.loadRes("Texture/Item/defaultSkill", function(err, data) {
+                    this.spriteFrame = new cc.SpriteFrame(data);
+                }.bind(this));
+            }
+        }.bind(this.node.getComponent(cc.Sprite)));
+    },
+    btn_clicked(){
+        var tmp=cc.find("Canvas");
+        var newpop=cc.instantiate(this.detial);
+        var newmask=cc.instantiate(this.maskPre);
+        console.log(newpop);
+        tmp.addChild(newmask);
+        newmask.addChild(newpop);
+        newmask.setPosition(0,0);
+        //var npc=Global.Game.npcs[this.labelPre.string];
+        //console.log(this.labelPre.string+"  "+curnpc+"   "+Global.Game.npcs["npc_002"])
+        newpop.getComponent('popscript').initdetail(this.skill);
+        //newpop.setPosition(380,64);
+    },
+    start () {
+
+    },
+
+    // update (dt) {},
+});
---
+++ assets/Script/PlayerSkillPage/popscript.js
@@ -0,0 +1,61 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+var Global = require("Global").storage;
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
+        head:{
+            default:null,
+            type:cc.Label
+        },
+        describe:{
+            default:null,
+            type:cc.Label
+        },
+        skill:null,
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+    closePop(){
+        this.node.parent.getComponent('mask').closeMask();
+        this.node.removeFromParent();
+    },
+    equip(){
+        Global.Player.skillset.equipSkill(this.skill.id);
+        this.closePop();
+    },
+    // onLoad () {},
+    initdetail(skill){
+        this.skill=skill;
+        this.head.string=skill.name;
+        this.describe.string=skill.description;
+    },
+    start () {
+
+    },
+
+    // update (dt) {},
+});
